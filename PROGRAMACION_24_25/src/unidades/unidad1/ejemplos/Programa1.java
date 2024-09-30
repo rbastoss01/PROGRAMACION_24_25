576f6b5bca4568767c513946a4c12d0576943cbc
diurno <diurno@192.168.0.113>
2024-09-30 09:54:57 +0200

    Modificado

--- PROGRAMACION_24_25/src/unidades/unidad1/ejemplos/Programa1.java
+++ PROGRAMACION_24_25/src/unidades/unidad1/ejemplos/Programa1.java
@@ -4,7 +4,7 @@ public class Programa1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("mensaje");
+		System.out.println("Modificado");
 	}
 
 }
